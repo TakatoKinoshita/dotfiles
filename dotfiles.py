88a0d57ca5089dde847925f9bb0f11cf601a8caa
TakatoKinoshita <takato@ieee.org>
2024-11-05 22:12:52 +0900

    add python script. I will go to ramen shop

---
+++ dotfiles.py
@@ -0,0 +1,14 @@
+import json
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+def main(
+        is_restore: bool = False,
+        is_dry_run: bool = False,
+):
+    ...
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument()

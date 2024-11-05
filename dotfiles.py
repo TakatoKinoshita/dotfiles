3fc56f2396b1ab4014de9cac22a7c0b48f893d34
TakatoKinoshita <takato@ieee.org>
2024-11-05 23:53:52 +0900

    make argparse

--- dotfiles.py
+++ dotfiles.py
@@ -1,7 +1,12 @@
 import json
+import sys
 
 from argparse import ArgumentParser
 from pathlib import Path
+from logging import getLogger, basicConfig, INFO
+
+LOGGER = getLogger(__name__)
+
 
 def main(
         is_restore: bool = False,
@@ -9,6 +14,16 @@ def main(
 ):
     ...
 
+
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument()
+    parser.add_argument(
+        "--restore", action="store_true",
+        help="Restore dotfiles from backup."
+    )
+    parser.add_argument(
+        "--dry-run", action="store_true",
+        help="Test run without actual file operations."
+    )
+    args = parser.parse_args()
+    main(args.restore, args.dry_run)

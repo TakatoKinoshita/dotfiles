7f4755f6180b2a5da96d20f0ee5cbdbbbf38f4dd
TakatoKinoshita <takato@ieee.org>
2024-11-06 22:40:37 +0900

    add check_convert_json

--- dotfiles.py
+++ dotfiles.py
@@ -9,10 +9,50 @@ from dataclasses import dataclass
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
-@dataclass
+@dataclass(frozen=True)
 class PathConfig:
     src: Path
     dst: Path
+
+json_scalar = bool | int | float | str
+json_type = json_scalar | list["json_obj"] | dict[str, "json_obj"]
+
+def check_convert_json(
+        src_base: Path,
+        home_dir: Path,
+        json_obj: json_type,
+) -> list[PathConfig]:
+    if isinstance(json_obj, json_scalar):
+        LOGGER.error("path.json must be JSON object or array of objects. But got %s.", json_obj)
+        raise RuntimeError("%s is invalid." % json_obj)
+
+    if isinstance(json_obj, dict):
+        json_obj = [json_obj]
+
+    path_list: list[PathConfig] = []
+    for conf in json_obj:
+        is_home = conf.get("is_home", True)
+        if not isinstance(is_home, bool):
+            LOGGER.error('value of key "is_home" must be boolean. But got %s.', is_home)
+            raise RuntimeError("%s is invalid." % is_home)
+
+        try:
+            src = src_base / Path(conf["src"])
+            dst = home_dir / Path(conf["dst"]) if is_home else Path(conf["dst"])
+        except KeyError:
+            LOGGER.error('key "src" or "dst" not found in "%s".', conf)
+            raise
+        except TypeError:
+            LOGGER.error('key "src" and "dst" must be valid path. But got "%s".', conf)
+            raise
+        except:
+            LOGGER.error("Unexpected error: %s", sys.exc_info()[0])
+            raise
+
+        path_list.append(PathConfig(src, dst))
+
+    return path_list
+
 
 def main(
         package_base: Path,
@@ -34,8 +74,14 @@ def main(
         setting_path = pack / "path.json"
         with open(setting_path) as f:
             LOGGER.debug("Opened %s.", setting_path)
-            path_dict = json.load(f)
-        LOGGER.debug("Loaded %s.", setting_path)
+            path_obj = json.load(f)
+            LOGGER.debug("Loaded %s.", path_obj)
+
+        path_config_list.extend(check_convert_json(
+            src_base=pack,
+            home_dir=home_dir,
+            json_obj=path_obj,
+        ))
 
 
 if __name__ == '__main__':

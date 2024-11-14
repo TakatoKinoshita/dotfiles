106d3d676e5da2b85efe0451f7d51537d919bb60
TakatoKinoshita <takato@ieee.org>
2024-11-14 14:06:44 +0900

    test for iter_package

---
+++ .test/tests/test_iter_package.py
@@ -0,0 +1,22 @@
+import unittest
+from pathlib import Path
+
+from dotfiles import iter_package
+from util import set_current_dir_to_test_root
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        set_current_dir_to_test_root()
+
+    def test_normal(self):
+        package_dir = Path("package_bases/for_iter")
+        res = list(iter_package(package_dir))
+        self.assertEqual(len(res), 2)
+        [self.assertIsInstance(e, Path) for e in res]
+        self.assertIn(package_dir / "pack1", res)
+        self.assertIn(package_dir / "pack2", res)
+
+
+if __name__ == '__main__':
+    unittest.main()

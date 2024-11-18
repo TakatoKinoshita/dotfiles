e1d3d1af92736e5d36642443303673bf016d207e
TakatoKinoshita <takato@ieee.org>
2024-11-18 23:44:51 +0900

    del util.load_jason use dotfiles.cache_load_json instead

--- .test/tests/util.py
+++ .test/tests/util.py
@@ -6,8 +6,3 @@ from pathlib import Path
 
 def set_current_dir_to_test_root():
     os.chdir(Path(__file__).parent.parent)
-
-def load_json(path):
-    with open(path, "r") as f:
-        json_data = json.load(f)
-    return json_data
